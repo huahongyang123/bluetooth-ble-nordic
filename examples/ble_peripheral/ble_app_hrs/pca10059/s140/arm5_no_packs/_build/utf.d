1bae391ed8c45c5dab6ea4f02a1481872bb613cc
“huahongyang” <2768639050@qq.com>
2024-09-17 23:18:42 +0800

    first-3

---
+++ examples/ble_peripheral/ble_app_hrs/pca10059/s140/arm5_no_packs/_build/utf.d
@@ -0,0 +1,4 @@
+.\_build\utf.o: ..\..\..\..\..\..\external\utf_converter\utf.c
+.\_build\utf.o: ..\..\..\..\..\..\external\utf_converter\utf.h
+.\_build\utf.o: D:\EmbeddedSoftware\MDK\MDK_FILE\ARM\ARMCC\Bin\..\include\stddef.h
+.\_build\utf.o: D:\EmbeddedSoftware\MDK\MDK_FILE\ARM\ARMCC\Bin\..\include\stdint.h
